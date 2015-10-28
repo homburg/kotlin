a00f308b42c0d736057653768e99d6395cf11b10
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-10-28 21:25:28 +0300

    Forbid import from root package (KT-9806)
     #KT-9806 Fixed

--- idea/src/org/jetbrains/kotlin/idea/quickfix/AutoImportFix.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/AutoImportFix.kt
@@ -41,9 +41,11 @@ import org.jetbrains.kotlin.idea.codeInsight.DescriptorToSourceUtilsIde
 import org.jetbrains.kotlin.idea.core.KotlinIndicesHelper
 import org.jetbrains.kotlin.idea.util.getResolutionScope
 import org.jetbrains.kotlin.idea.core.isVisible
+import org.jetbrains.kotlin.idea.imports.importableFqName
 import org.jetbrains.kotlin.idea.project.ProjectStructureUtil
 import org.jetbrains.kotlin.idea.util.CallTypeAndReceiver
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.parentOrNull
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.isImportDirectiveExpression
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
@@ -168,10 +170,11 @@ internal abstract class AutoImportFixBase<T: KtExpression>(expression: T, val di
 
         result.addAll(indicesHelper.getCallableTopLevelExtensions({ it == nameStr }, callTypeAndReceiver, element, bindingContext))
 
-        return if (result.size > 1)
-            reduceCandidatesBasedOnDependencyRuleViolation(result, file)
+        val noRootVariants = result.filterNot { it.importableFqName?.parentOrNull()?.isRoot ?: true }
+        return if (noRootVariants.size > 1)
+            reduceCandidatesBasedOnDependencyRuleViolation(noRootVariants, file)
         else
-            result
+            noRootVariants
     }
 
     private fun reduceCandidatesBasedOnDependencyRuleViolation(
