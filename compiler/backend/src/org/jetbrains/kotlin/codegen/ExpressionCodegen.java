9c2dc3a46b076b4e868703e58770f723be0b1d92
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-10-29 22:03:58 +0300

    Remove obsolete SCRIPT markers

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -2577,7 +2577,6 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
             }
         }
         else if (receiverValue instanceof ScriptReceiver) {
-            // SCRIPT: generate script
             return generateScript((ScriptReceiver) receiverValue);
         }
         else if (receiverValue instanceof ExtensionReceiver) {
@@ -2596,7 +2595,6 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
         return context.generateReceiver(descriptor, state, false);
     }
 
-    // SCRIPT: generate script, move to ScriptingUtil
     private StackValue generateScript(@NotNull ScriptReceiver receiver) {
         CodegenContext cur = context;
         StackValue result = StackValue.LOCAL_0;
@@ -3388,7 +3386,6 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
         Type varType = asmType(variableDescriptor.getType());
 
         StackValue storeTo;
-        // SCRIPT: Variable at the top of the script is generated as field
         if (KtPsiUtil.isScriptDeclaration(variableDeclaration)) {
             KtScript scriptPsi = KtPsiUtil.getScript(variableDeclaration);
             assert scriptPsi != null;
